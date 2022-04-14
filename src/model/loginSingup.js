f40d5c4618d7f8bf119fc2fcac63cf068eb31726
Arpit <97335788+fabpot11@users.noreply.github.com>
2022-04-14 07:56:27 +0530

    Update loginSingup.js

--- src/model/loginSingup.js
+++ src/model/loginSingup.js
@@ -1,10 +1,11 @@
 const mongoose = require('mongoose')
 
 const lsSchema = mongoose.Schema({
-    email:{type: String, require:true},
+    email:{type: String, require:true, unique: true},
     password : {type :String, require: true}
 },{
-    versionKey:false
+    versionKey:false,
+    timestamps: true
 })
 
 module.exports = mongoose.model('Userinfo',lsSchema)
