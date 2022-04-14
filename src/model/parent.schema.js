0a380d854e6742c33aadaeab4fd3685408bc52d4
Arpit <97335788+fabpot11@users.noreply.github.com>
2022-04-14 07:57:03 +0530

    Update parent.schema.js

--- src/model/parent.schema.js
+++ src/model/parent.schema.js
@@ -3,11 +3,12 @@ const mongoose = require("mongoose");
 const parentSchema = mongoose.Schema(
   {
     name: { type: String, require: true },
-    img: { type: String, require: true },
-    userId: { type: mongoose.Schema.Types.ObjectId , ref:'Userinfo', requied:true}
+      img: { type: String, require: true },
+      userId: { type: mongoose.Schema.Types.ObjectId , ref:'Userinfo', requied:true}
   },
   {
     versionKey: false,
+    timestamps: true
   }
 );
 
